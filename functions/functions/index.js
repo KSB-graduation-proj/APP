562192aad4c3b43045eb1b6fc72c3a34ac779a43
hyunsooBang <suusuu16@ewhain.net>
2023-04-06 20:36:02 +0900

    axios
    일단 디플로이가 안됨

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,42 +1,29 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const fetch = require("node-fetch");
+const axios = require("axios");
 
 admin.initializeApp();
 
 const orderF = require("./order");
 const paymentF = require("./payment");
+const refundF = require("./refund");
 
 exports.qrDetection = functions.firestore
     .document("qr/{documentId}")
-    .onCreate((snap, context) => {
-    // 새로운 문서가 추가될 때마다 실행되는 코드
+    .onCreate(async (snap, context) => {
       console.log("New document added: ", snap.id, snap.data());
       const data = {
         id: snap.id,
         data: snap.data(),
       };
-      // 라즈베리파이의 IP 주소와 포트번호
-      const raspberryPiIp = "192.168.137.202";
-      const port = 8000;
-      // 라즈베리파이에 POST 요청 보내기
-      return fetch(`http://${raspberryPiIp}:${port}/api/qr-detection`, {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {"Content-Type": "application/json"},
-      })
-          /* .then((res) => {
-            console.log(`statusCode: ${res.status}`);
-            return res.text();
-          })
-          .then((body) => {
-            console.log(body);
-            return null;
-          })*/
-          .catch((err) => {
-            console.error(err);
-            return null;
-          });
+      try {
+        const response = await axios.post("http://192.168.137.202:8000/api/qr-detection", data);
+        console.log("Response:", response.data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
     });
+
 exports.orderFunction = orderF.orderFunction;
 exports.paymentFunction = paymentF.paymentFunction;
+exports.refundFunction = refundF.refundFunction;
